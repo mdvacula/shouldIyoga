5427a71137ed431f4a101b977abb794785284648
mdvacula <mdvacula@gmail.com>
2018-01-31 15:52:36 -0500

    Color pick by moving mouse and clicking

--- src/index.js
+++ src/index.js
@@ -4,17 +4,34 @@ import css from '../assets/css/style.css'
 let colorPicker = document.getElementById("moodColor");
 let gtyDiv = document.getElementById("gtyBox");
 let isHidden;
-
+let col;
  gtyDiv.style.display = "none";
  isHidden = true;
 
-let watchColorPicker = (event) => {
-    var pickedColor = event.target.value;
+
+let processMouse = (event) => {
+    let r = (window.event.clientX /255)*100;
+    let g = (window.event.clientY /255)*100;
+    let b = (r/g)*10;
+
+    col = `rgb(${r.toFixed(0)},${g.toFixed(0)},${b.toFixed(0)})`;
+    // console.log(col);
+   document.body.style.backgroundColor = col;
+}
+
+let pickColor = (event) => {
+    console.log(col);
+    var pickedColor = col;
     var textColor = hello(pickedColor);
    
     stylePage(pickedColor, textColor.color)
     showDiv();
 }
+
+window.onmousemove = processMouse;
+window.onmousedown = pickColor;
+ 
+
 
 let stylePage = (body, text) =>{
     document.body.style.color = body;
